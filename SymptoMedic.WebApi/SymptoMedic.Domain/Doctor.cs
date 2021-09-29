0c5c0255adf7699adae6291374a26e939cf3b805
Chris Mesidor <christopher.mesidor1996@gmail.com>
2021-09-29 18:01:50 -0400

    fixing type errors

--- SymptoMedic.WebApi/SymptoMedic.Domain/Doctor.cs
+++ SymptoMedic.WebApi/SymptoMedic.Domain/Doctor.cs
@@ -5,7 +5,7 @@ namespace SymptoMedic.Domain
     public class Doctor
     {
         public Doctor(){}
-        public Doctor(int id, string firstname, string lastname, int? license, string practicename, string email, int? phonenumber,int? doctorspecialty, string practiceadress, string practicecity, string practicestate, int? practicezipcode, string certifications, string education, string sex)
+        public Doctor(int id, string firstname, string lastname, string license, string practicename, string email, string phonenumber,string doctorspecialty, string practiceadress, string practicecity, string practicestate, int? practicezipcode, string certifications, string education, string gender)
         {
             this.Id = id;
             this.FirstName = firstname;
@@ -21,24 +21,25 @@ namespace SymptoMedic.Domain
             this.PracticeZipcode = practicezipcode;
             this.Certifications = certifications;
             this.Education = education;
-            this.Sex = sex;
+            this.Gender = gender;
         }
 
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int? License { get; set; }
+        public string License { get; set; }
         public string PracticeName { get; set; }
         public string Email { get; set; }
-        public int? PhoneNumber { get; set; }
-        public int? DoctorSpeciality { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string DoctorSpeciality { get; set; }
         public string PracticeAddress { get; set; }
         public string PracticeCity { get; set; }
         public string PracticeState { get; set; }
         public int? PracticeZipcode { get; set; }
         public string Certifications { get; set; }
         public string Education { get; set; }
-        public string Sex { get; set; }
+        public string Gender { get; set; }
 
         //public virtual ICollection<Appointment> Appointments { get; set; }
         //public virtual ICollection<DoctorSymptom> DoctorSymptoms { get; set; }
