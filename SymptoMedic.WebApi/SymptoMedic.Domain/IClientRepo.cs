cec0a3829b472d494acba4780296677e4b26735e
Chris Mesidor <christopher.mesidor1996@gmail.com>
2021-09-30 17:06:00 -0400

    Making the outline for the Doctor Repo

--- SymptoMedic.WebApi/SymptoMedic.Domain/IClientRepo.cs
+++ SymptoMedic.WebApi/SymptoMedic.Domain/IClientRepo.cs
@@ -9,6 +9,10 @@ namespace SymptoMedic.Domain
     public interface IClientRepo
     {
         Task<List<Client>> GetClients();
-
+        Task<Client> AddAClient(Client client);
+        Task<Client> UpdateClient(int id, Client client);
+        Task<bool> DeleteClientById(int id);
+        Task<Client> ClientLoginAsync(Client user);
+        bool UniqueEmail(string email);
     }
 }
