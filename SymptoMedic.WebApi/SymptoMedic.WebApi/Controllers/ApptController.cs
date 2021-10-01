7562cd4177e93dbcba74e41eb8b8d6cb80b82ab5
Chris Mesidor <christopher.mesidor1996@gmail.com>
2021-10-01 11:14:12 -0400

    Adding repos and loggers to controllers

--- SymptoMedic.WebApi/SymptoMedic.WebApi/Controllers/ApptController.cs
+++ SymptoMedic.WebApi/SymptoMedic.WebApi/Controllers/ApptController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SymptoMedic.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,14 @@ namespace SymptoMedic.WebApi.Controllers
     [ApiController]
     public class ApptController : ControllerBase
     {
+        private readonly ILogger<ApptController> _logger;
+        private readonly IClientRepo _crepo;
+        private readonly IDoctorRepo _drepo;
+        public ApptController(ILogger<ApptController> logger, IClientRepo crepo, IDoctorRepo drepo)
+        {
+            _logger = logger;
+            _crepo = crepo;
+            _drepo = drepo;
+        }
     }
 }
