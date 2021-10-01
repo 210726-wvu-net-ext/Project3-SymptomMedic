9d5c881c26694bffba115a956ae8d6357e6fee73
Chris Mesidor <christopher.mesidor1996@gmail.com>
2021-10-01 11:34:34 -0400

    Made Doctor Controller

---
+++ SymptoMedic.WebApi/SymptoMedic.WebApi/Controllers/DoctorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SymptoMedic.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SymptoMedic.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly ILogger<DoctorController> _logger;
+        private readonly IDoctorRepo _drepo;
+        public DoctorController(ILogger<DoctorController> logger, IDoctorRepo drepo)
+        {
+            _logger = logger;
+            _drepo = drepo;
+        }
+    }
+}
