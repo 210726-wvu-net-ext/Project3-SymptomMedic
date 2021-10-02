57c199971c2bc6a765d00877baf12f0fd8f0ba9e
Rearles <53582049+Rearles@users.noreply.github.com>
2021-10-01 19:44:19 -0600

    Added more auth stuff

--- SymptomaticApp-UI/src/app/app.component.ts
+++ SymptomaticApp-UI/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SymptomaticApp-UI';
+  title = 'FourPatientApp-UI';
+  constructor(public auth: AuthService, private http: HttpClient) { }
+
 }
