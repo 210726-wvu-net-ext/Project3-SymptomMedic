0a83e812d34e9a3ad38cbc36a8e1d5e165c021f1
Rearles <53582049+Rearles@users.noreply.github.com>
2021-10-06 14:20:07 -0600

    Incorporation of auth service functions for booking appointments

--- SymptomaticApp-UI/src/app/auth.service.ts
+++ SymptomaticApp-UI/src/app/auth.service.ts
@@ -8,6 +8,7 @@ import { Client } from './interfaces/client';
 import { Doctor } from './interfaces/doctor';
 import { Router } from '@angular/router';
 import { IUser } from './interfaces/IUser';
+import { Appointment } from './interfaces/appointments';
 
 @Injectable({
   providedIn: 'root'
@@ -27,10 +28,45 @@ export class AuthService {
     email: null ||  this.decodedToken?.email,
     role: null ||  this.decodedToken?.role,
   }
-
-
+  currentAppointment: Appointment = {
+    id: null || this.decodedToken?.appointmentId,
+    dateCreated: null || this.decodedToken?.dateCreated,
+    clientId: null || this.decodedToken?.clientId,
+    doctorId: null || this.decodedToken?.doctorId,
+    clientFirstName: null || this.decodedToken?.clientFirstName,
+    clientLastName: null || this.decodedToken?.clientLastName,
+    clientContact: null || this.decodedToken?.clientContact,
+    patientSymptoms: null || this.decodedToken?.patientSymptoms,
+    startTime: null || this.decodedToken?.startTime,
+    endTime: null || this.decodedToken?.endTime,
+    client: null || this.decodedToken?.client,
+    doctor: null || this.decodedToken?.doctor,
+  }
+  
   constructor(private http: HttpClient, private router: Router) { }
 
+  setAppointment(data: any): Observable<Appointment> {
+    return this.http.post(`${baseUrl}auth/Appointment`, data)
+      .pipe(
+        map((response: any) => {
+          const decodeToken = this.helper.decodeToken(response.token);
+          this.currentAppointment.id = decodeToken.appointmentId;
+          this.currentAppointment.dateCreated = decodeToken.dateCreated;
+          this.currentAppointment.clientId = decodeToken.clientId;
+          this.currentAppointment.doctorId = decodeToken.doctorId;
+          this.currentAppointment.clientFirstName = decodeToken.clientFirstName;
+          this.currentAppointment.clientLastName = decodeToken.clientLastName;
+          this.currentAppointment.clientContact = decodeToken.clientContact,
+          this.currentAppointment.patientSymptoms = decodeToken.patientSymptoms;
+          this.currentAppointment.startTime = decodeToken.startTime;
+          this.currentAppointment.endTime = decodeToken.endTime;
+          this.currentAppointment.client = decodeToken.client;
+          this.currentAppointment.doctor = decodeToken.doctor;
+          localStorage.setItem('jwt', response.token);
+          return this.currentAppointment;
+        })
+      );
+  }
   loginClient(data: any): Observable<IUser> {
     return this.http.post(`${baseUrl}auth/LoginClient`, data)
       .pipe(
