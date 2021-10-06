ab87f050b1c4ffd460b837ce95fe6af806011798
Rearles <53582049+Rearles@users.noreply.github.com>
2021-10-06 14:19:22 -0600

    Start of setting up appointment service

--- SymptomaticApp-UI/src/app/appointment.service.ts
+++ SymptomaticApp-UI/src/app/appointment.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { observable, Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { baseUrl } from 'src/environments/environment';
+import { Appointment } from './interfaces/appointments';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  addAppointment(appointment: Appointment): Observable<Appointment> {
+    return this.http.post<>
+  }
 }
