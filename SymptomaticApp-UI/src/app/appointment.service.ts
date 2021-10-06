f132a1dc502a921a52d23c2624420488d74f0088
Rearles <53582049+Rearles@users.noreply.github.com>
2021-10-06 15:40:58 -0600

    Add get appointment and error handling

--- SymptomaticApp-UI/src/app/appointment.service.ts
+++ SymptomaticApp-UI/src/app/appointment.service.ts
@@ -29,8 +29,40 @@ export class AppointmentService {
   }
 
 
+  getAppointment(id: number): Observable<Appointment> {
+    const url = `${this.appointmentUrl}/${id}`;
+    return this.http.get<Appointment>(url)
+      .pipe(
+        //tap(_ => this.log(`fetched Appointment id=${id}`)),
+        catchError(this.handleError<Appointment>(`getAppointment id={id}`))
+      );
+
+  }
+  /** GET: gets an Appointment on the server*/
+
   handleError1(error: HttpErrorResponse) {
 
     return throwError(error.error);
   }
+  /**
+* Handle Http operation that failed.
+* Let the app continue.
+* @param operation - name of the operation that failed
+* @param result - optional value to return as the observable result
+*/
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      //this.log(`${operation} failed: ${error.message}`);
+      console.log(operation); //create message service
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
 }
